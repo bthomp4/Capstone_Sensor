46d33a6586e0d0dec476bc52ad4088e3450b0c7a
Ben Thompson <bthomp4@umbc.edu>
2017-12-14 23:42:01 -0500

    Arduino sensor code

---
+++ Ultrasonic_sensor_weatherproof.ino.ino
@@ -0,0 +1,24 @@
+/*
+ * This code was sourced from https://www.dfrobot.com/wiki/index.php/Weather-proof_Ultrasonic_Sensor_SKU_:_SEN0207
+ */
+
+#define ECHOPIN 10// Pin to receive echo pulse
+#define TRIGPIN 9// Pin to send trigger pulse
+void setup(){
+  Serial.begin(9600);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
+  digitalWrite(ECHOPIN, HIGH);
+}
+void loop(){
+  digitalWrite(TRIGPIN, LOW); // Set the trigger pin to low for 2uS
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN, HIGH); // Send a 10uS high to trigger ranging
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW); // Send pin low again
+  int distance = pulseIn(ECHOPIN, HIGH,26000); // Read in times pulse
+  distance= distance/58;
+  Serial.print(distance);
+  Serial.println("   cm");                    
+  delay(50);// Wait 50mS before next ranging
+}
